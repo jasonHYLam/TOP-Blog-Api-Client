7a27e4185393ee3b86e385b9c7943d7a145c0770
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-06 21:00:04 +0000

    Create App Skeleton And Post Component

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+import { Post } from './Components/Post'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('')
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/home')
+    .then((res) => res.json())
+    .then((data) => () => {
+      setMessage(data.message)
+      // setPosts(data.allPosts)
+    })
+    
+  }, [])
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/create_new_post')
+  //   .then((res) => res.json())
+  //   .then((data) => setMessage(data.message))
+  // }, [])
+
+  console.log(message);
+  // console.log(posts);
+
+  return (
+    <>
+      <h1>Ho there</h1>
+      <Post/>
+
+      <div className='App'>
+        <h1>message: {message}</h1>
+
+        {posts.map(post => {
+          <li>{post}</li>
+        })}
+
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Post.jsx
@@ -0,0 +1,6 @@
+export function Post() {
+    return <div>
+        <h1>This is a post</h1>
+    </div>
+}
+
