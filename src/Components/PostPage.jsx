2657956b7047a5ec30379d78eed75f004617b16c
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-09 17:30:29 +0000

    Use UseLoaderData To Get Data In PostPage Element

--- src/Components/PostPage.jsx
+++ src/Components/PostPage.jsx
@@ -1,9 +1,15 @@
+import { useLoaderData } from "react-router-dom"
+
 export function PostPage() {
     // should i use useState and useEffect? well i certainly need to fetch the data.
     // should i use the loader function...?
+    const postData = useLoaderData();
+    console.log(postData)
     return (
+
         <>
             <p>Hope this works</p>
+
         
         </>
     )
