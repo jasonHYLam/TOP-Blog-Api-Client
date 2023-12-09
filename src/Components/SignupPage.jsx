b161d196561afe4f3659681c669d3bf90ca1d4d5
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-09 19:41:24 +0000

    Create Signup Form Page Skeleton

--- src/Components/SignupPage.jsx
+++ src/Components/SignupPage.jsx
@@ -1,8 +1,16 @@
 export function SignupPage() {
     return (
         <>
-        <div>
-        </div>
+            <main>
+                <h1>Sign up!</h1>
+                <form action="">
+                    <label htmlFor="">Username (min. 4 chars)</label>
+                    <input type="text" />
+
+                    <label htmlFor="">Password (min. 5 chars)</label>
+                    <input type="password" />
+                </form>
+            </main>
         
         </>
     )
