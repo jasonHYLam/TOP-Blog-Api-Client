1852ad8b52d75bb021ab64b9b0c7e38a9e687c5e
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-09 19:02:46 +0000

    Show Post And Comments Data On PostPage Component

--- src/Components/PostPage.jsx
+++ src/Components/PostPage.jsx
@@ -3,12 +3,30 @@ import { useLoaderData } from "react-router-dom"
 export function PostPage() {
     // should i use useState and useEffect? well i certainly need to fetch the data.
     // should i use the loader function...?
-    const postData = useLoaderData();
-    console.log(postData)
+    const {post, comments} = useLoaderData();
+    console.log(comments)
     return (
 
         <>
             <p>Hope this works</p>
+
+            <h1>{post.title}</h1>
+            <p>{post.text}</p>
+
+            <hr />
+            <p>Comments</p>
+            {comments.map(comment => {
+                return (
+                    <>
+                        <div key={comment._id}>
+                            <p>{comment.author}</p>
+                            <p>{comment.text}</p>
+                            <p>{comment.timeStamp}</p>
+                        </div>
+                        <hr />
+                    </>
+                )
+            })}
 
         
         </>
