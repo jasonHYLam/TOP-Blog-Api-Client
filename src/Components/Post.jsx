20cc178b27565119b13edfd90743883d1dad23da
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-09 19:22:37 +0000

    Use Article Tag In Post Component

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom"
 export function Post({ post }) {
     return <>
         <Link to={`/posts/${post._id}`}>
-            <div>
+            <article>
                 <p>How about a meal?</p>
                 <h1>{ post.title }</h1>
                 <p>{ post.text }</p>
-            </div>
+            </article>
         </Link>
     </>
 }
--- src/main.jsx
+++ src/main.jsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
       },
       {
         path: 'sign-up',
-        element:,
+        // element:,
       }
     ],
     errorElement: <ErrorPage/>
