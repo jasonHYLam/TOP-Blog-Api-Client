ce4c07d18b8f6222a14f37de9078fb4afa5a02da
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-10 16:48:35 +0000

    Use Form Component For SignupPage

--- src/Components/SignupPage.jsx
+++ src/Components/SignupPage.jsx
@@ -1,16 +1,17 @@
+import { Form } from "react-router-dom"
 export function SignupPage() {
     return (
         <>
             <main>
                 <h1>Sign up!</h1>
-                <form action="">
-                    <label htmlFor="">Username (min. 4 chars)</label>
-                    <input type="text" />
+                <Form method="POST">
+                    <label htmlFor="username">Username (min. 4 chars)</label>
+                    <input name="username" type="text" required minLength={4}  />
 
-                    <label htmlFor="">Password (min. 5 chars)</label>
-                    <input type="password" />
+                    <label htmlFor="password">Password (min. 5 chars)</label>
+                    <input name="password" type="password" required minLength={5} />
                     <button type="submit"> Create account</button>
-                </form>
+                </Form>
             </main>
         
         </>
