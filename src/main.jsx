184d94075b19540b58e3bb4d8dd62dd009bf121b
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-17 17:48:12 +0000

    Add Credentials: Include For PostPage Fetch

--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,12 @@ const router = createBrowserRouter([
         element: <PostPage/>,
         // I may need try and catch...
         loader: async ({params}) => {
-          return await fetch(`http://localhost:3000/home/${params.postId}`)
+          return await fetch(
+            `http://localhost:3000/home/${params.postId}`,
+            {
+              credentials: "include"
+            }
+            )
         },
       },
       {
