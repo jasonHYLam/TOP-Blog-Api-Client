224872cc4f7cab44980a38224907b35cc77d0395
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-31 13:41:15 +0000

    Add Border To PostPreview Component

--- src/Components/PostPreview/PostPreview.jsx
+++ src/Components/PostPreview/PostPreview.jsx
@@ -1,3 +1,5 @@
+import styles from './PostPreview.module.css';
+
 import { Link } from 'react-router-dom';
 
 export function PostPreview({post}) {
@@ -8,7 +10,8 @@ export function PostPreview({post}) {
     // maybe show number of likes
     return (
         <>
-            <Link to={`/posts/${post._id}`}>
+
+            <Link className={styles.postContainer} to={`/posts/${post._id}`}>
                 <p>{post.title}</p>
                 <p>{post.date}</p>
                 <p>{post.author.username}</p>
