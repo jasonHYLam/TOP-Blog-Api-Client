5cb940bf0763c79f2783c786d479c8b4211d1a8f
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-02 17:04:48 +0000

    Fix Undefined User In Header Error After Login
    This was an error caused by checking user.username exists, when instead
    user should have been checked.

--- src/Components/PageHeader/PageHeader.jsx
+++ src/Components/PageHeader/PageHeader.jsx
@@ -1,16 +1,25 @@
 import { Link } from "react-router-dom"
 import styles from './PageHeader.module.css';
+import { useEffect, useState } from "react";
 
 // isUserLoggedIn is a state variable from PageLayout, which determines if the user is logged in and conditionally renders the corresponding links.
 export function PageHeader({isUserLoggedIn, user}) {
 
+    const [ isLoaded, setIsLoaded ] = useState(false);
+    useEffect(() => {
+        setIsLoaded(true)
+    });
+
     return (
+        
+        !isLoaded ? <p>Loading...</p> :
         <>
             <header className={styles.header}>
                 <div className={styles.linksGroup}>
                     {isUserLoggedIn ? 
                     <>
-                        {user.username ?  <p className={styles.headerText}>Welcome, {user.username}</p> : null}
+
+                        {user ?  <p className={styles.headerText}>Welcome, {user.username}</p> : null}
                         <Link className={styles.headerText} to={"posts"}>Home</Link>
                         <Link className={styles.headerText} to={"/logout"}>Log out</Link>
                     </>
