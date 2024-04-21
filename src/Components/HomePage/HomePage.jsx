45a3f0b29c1c3bc0c4f05f049d00ae04ec7bff04
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-04-21 21:12:20 +0100

    Make Posts Grid Stretch Entire Parent

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react'
-import { PostPreview } from '../PostPreview/PostPreview';
-import styles from './HomePage.module.css'
+import { useState, useEffect } from "react";
+import { PostPreview } from "../PostPreview/PostPreview";
+import styles from "./HomePage.module.css";
 
 // could include number of comments and likes.
 
@@ -9,29 +9,24 @@ export function HomePage() {
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_BACKEND_URL}/home`)
-    .then((res) => res.json())
-    .then((data) => setPosts(data.allPosts))
-    .then()
-  }, [])
+      .then((res) => res.json())
+      .then((data) => setPosts(data.allPosts))
+      .then();
+  }, []);
 
-    return (
-        <>
-            <p>Yo ho, land spotted.</p>
-            {/* show only the published blogPosts */}
-            <section className={styles.postsGroup}>
-
-              {posts.map((post) => {
-                if (post.published_status) {
-
-                  return (
-                    <PostPreview key={post._id} post={post}/>
-                  )  
-
-                }
-            })}
-
-            </section>
-            
-        </>
-    )
+  return (
+    <>
+      <main>
+        <p>Yo ho, land spotted.</p>
+        <section className={styles.postsGroup}>
+          {/* show only the published blogPosts */}
+          {posts.map((post) => {
+            if (post.published_status) {
+              return <PostPreview key={post._id} post={post} />;
+            }
+          })}
+        </section>
+      </main>
+    </>
+  );
 }
